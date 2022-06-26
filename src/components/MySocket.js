110f213a3a3fa557dca4c42fc9c4e524d281120b
yash nirmal <yash15nirmal@gmail.com>
2022-06-26 15:36:41 +0530

    final

--- src/components/MySocket.js
+++ src/components/MySocket.js
@@ -1,6 +1,8 @@
 import { io } from "socket.io-client";
 
-const mSocket = io("https://crisscross-pvp-backend.herokuapp.com/");
+// const mSocket = io("https://crisscross-pvp-backend.herokuapp.com/");
+const mSocket = io("http://localhost:8000/");
+
 mSocket.on("connect",()=>{
     console.log(mSocket.id);
 })
